913577461d3986abbce32ca79fa2cd9b6b57cd0d
Laboratorios <Laboratorios@V07>
2019-10-17 19:19:30 -0600

    Trabajo NoSQL

---
+++ Mongo2/Mongo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo2
+{
+    class Program
+                         public Utiles.Class1 MyProperty { get; set; }
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
