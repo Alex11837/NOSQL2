8f4b0570f307e96b5b171d59248af58084058707
Laboratorios <Laboratorios@V07>
2019-10-17 19:29:04 -0600

    Actualizado carpeta utiles

---
+++ Mongo2/Mongo2/Utiles/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongo2.Utiles
+{
+    class Class1
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+    }
+}
